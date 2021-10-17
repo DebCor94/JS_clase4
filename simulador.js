e912b95df8a1e6d6d0d964d5c5b1acea8882659b
DebCor94 <debora.coronel94@gmail.com>
2021-10-17 19:10:10 -0300

    correcciones

--- simulador.js
+++ simulador.js
@@ -10,66 +10,69 @@ DESCUENTO GRUPO FAMILIAR (3 o más personas) = $500 sobre el total de la compra
 
 */
 
-
 let total = 0;
 let ticket = 0;
 let invitados = 0;
 let precio = 0;
-let cuponDescuento 
+let cuponDescuento;
 let descuento = 0;
 
+function seleccionarTicket() {
+  let ticket = prompt(
+    "Elige tu tipo de pase: bronce, plata u oro",
+    "Oro"
+  ).toUpperCase();
+  let invitados = parseInt(
+    prompt("Selecciona la cantidad de tickets que deseas")
+  );
+  let precio = 0;
 
-function seleccionarTicket () {
-        let ticket = prompt("Elige tu tipo de pase: bronce, plata u oro", "Oro").toUpperCase();
-        let invitados = parseInt(prompt("Selecciona la cantidad de tickets que deseas"));
-        let precio = 0;
-
-        switch (ticket) {
-            case "BRONCE":
-                precio = 1000;
-                break;
-            case "PLATA":
-                precio = 1500;
-                break;
-            case "ORO":
-                precio = 2000;
-                break;
-        }
-        total = precio * invitados;
-        alert ("El subtotal es" + " " + total + " " + "ARS");
+  switch (ticket) {
+    case "BRONCE":
+      precio = 1000;
+      break;
+    case "PLATA":
+      precio = 1500;
+      break;
+    case "ORO":
+      precio = 2000;
+      break;
+  }
+  total = precio * invitados;
+  alert("El subtotal es" + " " + total + " " + "ARS");
 }
 
-function agregarCupon () {
-    cuponDescuento = prompt("¿Querés agregar un cupón de descuento?", "Si").toUpperCase();
-    if (cuponDescuento == "SI" && cuponDescuento != "") {
-        descuento = total - total*0.20;
-        alert ("El subtotal de tu compra es" + " " + descuento);
+function agregarCupon() {
+  cuponDescuento = prompt(
+    "¿Querés agregar un cupón de descuento?",
+    "Si"
+  ).toUpperCase();
+  if (cuponDescuento == "SI" && cuponDescuento != "") {
+    descuento = total - total * 0.2;
+    alert("El subtotal de tu compra es" + " " + descuento);
 
-        return descuento;
-    }
-    else {
-        alert ("No ha agregado descuentos a su pase")
-    }
+    return descuento;
+  } else {
+    alert("No ha agregado descuentos a su pase");
+  }
 }
 
-function calcularIva () {
-    if (cuponDescuento == "SI") {
-        total = descuento + descuento*0.21;
-        alert ("El precio final de tu compra es" + " " + total)
+function calcularIva() {
+  if (cuponDescuento == "SI") {
+    total = descuento + descuento * 0.21;
+    alert("El precio final de tu compra es" + " " + total);
 
-        return total;
-    } else {
-        total = total + total*0.21;
-        alert("El precio final es:" + " " + total)
+    return total;
+  } else {
+    total = total + total * 0.21;
+    alert("El precio final es:" + " " + total);
 
-        return total;
-    }  
+    return total;
+  }
 }
 
-alert ("¡Bienvenido a Race Park!");
+alert("¡Bienvenido a Race Park!");
 
-seleccionarTicket ();
-agregarCupon ();
-calcularIva ();
-
-
+seleccionarTicket();
+agregarCupon();
+calcularIva();
